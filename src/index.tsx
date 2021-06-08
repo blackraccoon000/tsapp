35f86be1f4624119ef167a2165b5be4727ae6824
Yutaka Fujii <yutakaf@playwell.site>
2021-06-08 16:12:05 +0900

    ref use types

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,18 @@
 import ReactDOM from 'react-dom';
 // import EventComponent from './events/EventComponent';
-import UserSearch from './classes/UserSearch';
+// import UserSearch from './classes/UserSearch';
+import UserSearch from './refs/UserSearch';
 
-const users = [
-  { name: 'Sarah', age: 20 },
-  { name: 'Alex', age: 20 },
-  { name: 'Michael', age: 20 },
-];
+// const users = [
+//   { name: 'Sarah', age: 20 },
+//   { name: 'Alex', age: 20 },
+//   { name: 'Michael', age: 20 },
+// ];
 
 const App = () => {
   return (
     <div>
-      <UserSearch users={users} />
+      <UserSearch />
     </div>
   );
 };
